8a4e5bd3a79750b8d10153bafc5d44b5cf610fa9
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2024-10-09 20:53:25 -0400

    chore: creat auth config file

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-import Credentials from 'next-auth/providers/credentials';
+import {authConfig } from '@/auth.config';
 import Google from 'next-auth/providers/google';
  
 export const { auth, signIn, signOut, handlers } = NextAuth({
